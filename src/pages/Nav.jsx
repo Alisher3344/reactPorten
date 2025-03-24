b795181788e8217dc7600e730919c8d4c0d00b83
Alisher3344 <alisher3344ali@gmail.com>
2025-03-24 22:26:02 +0500

    fixed a and li

--- src/pages/Nav.jsx
+++ src/pages/Nav.jsx
@@ -1,18 +1,22 @@
 const Nav = () => (
-  <nav >
+  <nav>
     <div className="container d-flex">
       <h1 className="h1Nav">PORTEN</h1>
       <div className="elem_right">
         <ul>
-          <li className="navLi">
-            <a href="#Bir">Понравилось</a>
-          </li>
-          <li className="navLi">
-            <a href="#Ikki">Личный</a>
-          </li>
-          <li className="navLi">
-            <a href="#Uch">Настройки</a>
-          </li>
+          <a className="navLi" href="#Bir">
+            {" "}
+            <li>Понравилось </li>
+          </a>
+
+          <a className="navLi" href="#Ikki">
+            <li>Личный </li>
+          </a>
+
+          <a className="navLi" href="#Uch">
+            {" "}
+            <li>Настройки </li>
+          </a>
         </ul>
         <div className="navLogos">
           <img src="karzinka.svg" alt="Cart" />
