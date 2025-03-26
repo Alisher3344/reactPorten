48389ef12223b5d399bb69ff0006a449451e9b6e
Alisher3344 <alisher3344ali@gmail.com>
2025-03-26 05:52:20 +0500

    back top

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,27 @@ import Footer from "./pages/Footer";
 import Section from "./pages/Section";
 
 import AOS from "aos";
-import { useEffect } from "react";
-import "aos/dist/aos.css"
+import { useEffect, useState } from "react";
+import "aos/dist/aos.css";
+
 function App() {
+  const [topClass, setTopClass] = useState("top");
+
   useEffect(() => {
     AOS.init({});
+
+    const handleScroll = () => {
+      if (window.scrollY > 200) {
+        setTopClass("top");
+      } else {
+        setTopClass("hidden");
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
   }, []);
+
   return (
     <>
       <Header />
@@ -21,6 +36,15 @@ function App() {
       <Section />
       <Card />
       <Footer />
+      <a href="#" style={{ position: "absolute" }}>
+        <div className={topClass} id="top">
+          <button className="button">
+            <svg viewBox="0 0 384 512" className="svgIcon">
+              <path d="M214.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 141.2V448c0 17.7 14.3 32 32 32s32-14.3 32-32V141.2L329.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160z"></path>
+            </svg>
+          </button>
+        </div>
+      </a>
     </>
   );
 }
--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,6 +1,6 @@
 const Header = () => (
   <header>
-    <div className="container d-flex">
+    <div className="container d-flex" id="top">
       <div className="header_flex">
         <img src="phone.svg" alt="Phone" />
         <p>8 (812) 123-45-67 | Работаем 7 дней в неделю | 9:00 — 18:00</p>
