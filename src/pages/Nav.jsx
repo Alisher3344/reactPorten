1ade979719cedb236289e8238318b052658fc005
Alisher3344 <alisher3344ali@gmail.com>
2025-03-27 22:56:37 +0500

    toogle

--- src/pages/Nav.jsx
+++ src/pages/Nav.jsx
@@ -1,29 +1,46 @@
-const Nav = () => (
-  <nav>
-    <div className="container d-flex">
-      <h1 className="h1Nav">PORTEN</h1>
-      <div className="elem_right">
-        <ul>
-          <a className="navLi" href="#Bir">
-            {" "}
-            <li>Понравилось </li>
-          </a>
+import { useState } from "react";
 
-          <a className="navLi" href="#Ikki">
-            <li>Личный </li>
-          </a>
+const Nav = () => {
+  const [isOpen, setIsOpen] = useState(false);
 
-          <a className="navLi" href="#Uch">
-            {" "}
-            <li>Настройки </li>
-          </a>
-        </ul>
-        <div className="navLogos">
-          <img src="karzinka.svg" alt="Cart" />
-          <img src="search.svg" alt="Search" />
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <nav>
+      <div className="container d-flex1">
+        <div className="flex_header">
+          <h1 className="h1Nav">PORTEN</h1>
+          <img
+            className="Hamburger"
+            src="hamburger.png"
+            alt="No icon"
+            onClick={toggleMenu}
+          />
+        </div>
+        <div className={`elem_right ${isOpen ? "open" : ""}`}>
+          <ul>
+            <a className="navLi" href="#Bir">
+              <li>Понравилось</li>
+            </a>
+
+            <a className="navLi" href="#Ikki">
+              <li>Личный</li>
+            </a>
+
+            <a className="navLi" href="#Uch">
+              <li>Настройки</li>
+            </a>
+          </ul>
+          <div className="navLogos">
+            <img src="karzinka.svg" alt="Cart" />
+            <img src="search.svg" alt="Search" />
+          </div>
         </div>
       </div>
-    </div>
-  </nav>
-);
+    </nav>
+  );
+};
+
 export default Nav;
